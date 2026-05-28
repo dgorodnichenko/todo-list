41bd89717cdd0904cb98d192617a8f1988c68746
dgorodnichenko <dgorodnichenko9@gmail.com>
2026-05-28 20:51:28 +0300

    feat(server): add update moethod for taskController and taskService

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -20,5 +21,13 @@ export class TasksController {
   @HttpCode(204)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.tasksService.remove(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateTaskDto: UpdateTaskDto
+) {
+    return this.tasksService.update(id, updateTaskDto);
   }
 }
--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './entity/task.entity';
 import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -30,5 +31,20 @@ export class TasksService {
     if (result.affected === 0) {
         throw new NotFoundException(`Task with id ${id} not found`);
     }
+  }
+
+  async update(id: string, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    if (Object.keys(updateTaskDto).length === 0) {
+      throw new BadRequestException('At least one field must be provided');
+    }
+
+    const task = await this.tasksRepository.findOneBy({ id });
+
+    if (!task) {
+      throw new NotFoundException(`Task with id "${id}" was not found`);
+    }
+
+    Object.assign(task, updateTaskDto);
+    return this.tasksRepository.save(task);
   }
 }
